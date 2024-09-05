61dad33d46b9246dae95ae586bb1f4e0cf788b3e
david.baker <davvidbaker@gmail.com>
2024-09-05 13:54:58 -0600

    remove invaliud export

--- src/app/optimizing/test-page/page.tsx
+++ src/app/optimizing/test-page/page.tsx
@@ -4,9 +4,6 @@ import { Heading } from "@/components/Heading"
 import codeBlock from "@/../public/images/optimizing/q1.gms"
 import { CodeSnippet, Constraints, DecisionVariables, Parameters, SetsAndIndices } from "@/components/optimization"
 
-export const k = (s) => s.raw[0]
-
-
 
 const Page = () => <div><Heading /><SetsAndIndices />
 
