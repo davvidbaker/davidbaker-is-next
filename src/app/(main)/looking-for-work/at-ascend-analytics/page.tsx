a343cbe022947ae767adebaed16fa364181ef4ac
david.baker <davvidbaker@gmail.com>
2024-11-11 17:57:05 -0700

    html page

---
+++ src/app/(main)/looking-for-work/at-ascend-analytics/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+import { Resume } from '../base';
+
+const objective = `Experienced software engineer with background in power systems seeking a full-stack developer position in grid analytics software.
+Professional experience in developing flexible enterprise software solutions and skilled in data analysis.
+Eager to contribute to cutting-edge projects by leveraging technical skills and a passion for innovative energy solutions.`
+
+
+const ResumeGridPlanning = () => <Resume
+    objective={objective}
+    relevantCoursework="Power Systems Analysis, Power Quality, Linear Optimization, Data Science, Computational Economics"
+/>
+
+export default ResumeGridPlanning
